b6c0c5a4d0f8638ab561dc6ba901e623c316b206
kakadyx <mintesok@gmail.com>
2023-08-30 00:59:33 +0300

    Сделал задачу getStringCount

--- 5/5.6 Symbol.iterator.js
+++ 5/5.6 Symbol.iterator.js
@@ -1,45 +1,43 @@
-let baseObj = {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-    [Symbol.iterator](){
-        return {
-            entries: Object.entries(this),
-            index: 0,
-            next(){
-                if(this.index !== this.entries.length){
-                    return {done: false, value: this.entries[this.index++]}
-                }
-                return {done: true}
-            }
+const baseObj = {
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
+  [Symbol.iterator]() {
+    return {
+      entries: Object.entries(this),
+      index: 0,
+      next() {
+        if (this.index !== this.entries.length) {
+          return { done: false, value: this.entries[this.index++] };
         }
-    }
+        return { done: true };
+      },
+    };
+  },
+};
+
+for (const item of baseObj) {
+  console.log(item);
 }
 
-for(let item of baseObj){
-    console.log(item)
+const endlessIteratingObj = {
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
+  [Symbol.iterator]() {
+    return {
+      entries: Object.entries(this),
+      index: 0,
+      next() {
+        return { done: false, value: Math.floor(Math.random() * 10) };
+      },
+    };
+  },
+};
+
+for (const item of endlessIteratingObj) {
+  console.log(item);
+  if (item <= 3 && item >= 1) { break; }
 }
 
-let endlessIteratingObj = {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-    [Symbol.iterator](){
-        return {
-            entries: Object.entries(this),
-            index: 0,
-            next(){
-                return {done: false, value: Math.floor(Math.random() * 10)}
-            }
-        }
-    }
-}
-
-for(let item of endlessIteratingObj){
-    console.log(item)
-    if(item <= 3 && item >= 1)
-        break
-}
-
-
-console.log(Array.from(baseObj))
+console.log(Array.from(baseObj));
---
+++ common/getStringCount.js
@@ -0,0 +1,39 @@
+const stringCountCacheMap = new WeakMap();
+function reduceCb(acc, currentValue) {
+  if (typeof currentValue === 'string') {
+    return acc + 1;
+  } if (typeof currentValue === 'object') return acc + getStringCount(currentValue);
+  return acc;
+}
+function getStringCount(obj) {
+  if (!obj) return 0;
+  if (typeof obj === 'object' && stringCountCacheMap.has(obj)) {
+    console.log('cached', stringCountCacheMap.get(obj));
+    return stringCountCacheMap.get(obj);
+  }
+  if (typeof obj === 'object') {
+    let values = null;
+    if (Array.isArray(obj)) {
+      values = obj;
+    } else {
+      values = Object.values(obj);
+    }
+    const count = values.reduce(reduceCb, 0);
+    stringCountCacheMap.set(obj, count);
+    return count;
+  }
+
+  return 0;
+}
+const itemToCheck = {
+  first: '1',
+  second: { prop: '2' },
+  third: false,
+  fourth: ['anytime', 2, 3, 4],
+  fifth: null,
+};
+const arrToCheck = ['1', '2', ['3', ['123', '231'], itemToCheck]];
+console.log(getStringCount(itemToCheck)); // 3
+console.log(getStringCount(itemToCheck)); // 3
+console.log(getStringCount(arrToCheck));
+console.log(getStringCount(arrToCheck));
