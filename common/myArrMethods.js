acffd6763332068c2e3596d0418651c265c4d5c2
kakadyx <mintesok@gmail.com>
2023-09-05 17:29:22 +0300

    up reduce

--- common/myArrMethods.js
+++ common/myArrMethods.js
@@ -12,6 +12,7 @@ const arr = [14, 2, 3, 4, 5];
 
 arr.__proto__.myForEach = myForEach;
 
+Array.prototype.re;
 arr.myForEach((item) => console.log(item));
 
 // myReduce
@@ -20,6 +21,8 @@ function myReduce(callback, acc) {
   let innerAcc = acc ?? this[0];
   for (const item of this) {
     i += 1;
+    // eslint-disable-next-line no-continue
+    if (innerAcc === this[0]) continue;
     innerAcc = callback(innerAcc, item, i, this);
   }
   return innerAcc;
