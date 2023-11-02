b21eebea9a4f099931861c2a726066dd08393a3d
kakadyx <mintesok@gmail.com>
2023-11-02 21:53:37 +0300

    1 версия функции для сериализации

--- common/Сериализатор инстанса класса.js
+++ common/Сериализатор инстанса класса.js
@@ -1,31 +1,56 @@
-// eslint-disable-next-line max-classes-per-file
-class Serializer {
-  static serialize(instance) {
-    // field
-    const classPrototype = instance.__proto__;
-    // base methods
-    const classMethods = Object.getOwnPropertyDescriptors(classPrototype);
-    // static methods
-    const classConstructor = Object.getOwnPropertyDescriptors(classPrototype.constructor);
-  }
+function serializeClassInstance(instance) {
+  const classPrototype = instance.__proto__;
 
-  static deserialize(string) {
+  const classConstructorDescriptors = Object.getOwnPropertyDescriptors(classPrototype.constructor);
+  const prototypeDescriptors = Object.getOwnPropertyDescriptors(classPrototype);
+  const instanceDescriptors = Object.getOwnPropertyDescriptors(instance);
 
+  const filteredMethodsEntries = Object.entries(prototypeDescriptors)
+    .filter((key) => key !== 'contructor');
+  const filteredStaticFieldsEntries = Object.entries(classConstructorDescriptors)
+    .filter(([key]) => !['name', 'prototype', 'length'].includes(key));
+  function reduceCallbackGetMethodsValues(acc, [key, descriptor]) {
+    if (descriptor.value) {
+      if (descriptor.value.length || !key.startsWith('get')) {
+        return acc;
+      }
+
+      acc[key] = descriptor.value.call(instance);
+
+      return acc;
+    }
+
+    acc[key] = descriptor.get.call(instance);
+    return acc;
   }
+  const fields = Object.fromEntries(
+    Object.entries(instanceDescriptors)
+      .map(([key, descriptor]) => [key, descriptor.value]),
+  );
+  const staticFields = filteredStaticFieldsEntries.reduce(reduceCallbackGetMethodsValues, {});
+  const methodsValues = filteredMethodsEntries.reduce(reduceCallbackGetMethodsValues, {});
+  return JSON.stringify({ ...methodsValues, ...staticFields, ...fields });
 }
 
 class Foo {
-  bar = 'foo';
+  field = 5;
 
-  foo() {
-    return this.bar;
+  get bar() {
+    return this.field;
   }
 
-  static foobar() {
-    return `${this.bar}foo`;
+  getBiba() {
+    return Foo.biba;
   }
+
+  static getStaticBiba() {
+    return 'bibastatic';
+  }
+
+  static biba = 'biba';
 }
 
-const foobar = new Foo();
+const a = new Foo();
 
-Serializer.serialize(foobar);
+console.log(serializeClassInstance(a));
+console.log(JSON.parse(serializeClassInstance(a)));
